740e1e22a4ecc4da14f2e7f60844e80b28a528ca
Abdikariim abdiaziiz mohamed <abdikariimabdiaziiz91@gmail.com>
2026-09-22 13:09:01 +0300

    Add Week 2 Simple Bill Calculator assignment

---
+++ bill_calculator.py
@@ -0,0 +1,4 @@
+price = float(input("Geli qiimaha hal shay (Price): "))
+quantity = int(input("Geli tirada aad rabto (Quantity): "))
+total = price * quantity
+print(f"{quantity} items at {price:.2f} each = {total:.2f}")
